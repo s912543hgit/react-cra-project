728683d0813e55a63305f192be9112977a5b7283
wang Angel <>
2023-02-06 21:47:29 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+//外部套件
+
+import logo from "./assets/logo.svg";
+import "./assets/App.css";
+import Input from "./components/Input";
+import "./assets/all.scss";
+//axios
 
 function App() {
+  const [text, setText] = useState("");
+  const onChangeHandler = (e) => {
+    setText(e.target.value);
+  };
+
+  useEffect(() => {
+    (async () => {
+      const path = process.env.REACT_APP_PATH;
+      const result = await axios.get(path);
+      console.log(result);
+    })();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>lorem ddddd</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +34,16 @@ function App() {
         >
           Learn React
         </a>
+        <button type="button" className="btn btn-primary">
+          Primary
+        </button>
+        {text}
+        <Input
+          id="sampleText"
+          text="這是一個input"
+          value={text}
+          onChangeHandler={onChangeHandler}
+        ></Input>
       </header>
     </div>
   );
